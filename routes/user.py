21a2a559ef8db6de041bc5c9669e2a4f27bd0a0f
Jesus Pernia <padron2991@gmail.com>
2023-03-07 09:12:35 -0500

    path operations update user and delete user create

--- routes/user.py
+++ routes/user.py
@@ -19,8 +19,8 @@ def get_users():
     return conn.execute(users.select()).fetchall()
 
 
-@user.get("/users/count", tags=["users"], response_model=UserCount)
-def get_users_count():
+#@user.get("/users/count", tags=["users"], response_model=UserCount)
+#def get_users_count():
     result = conn.execute(select([func.count()]).select_from(users))
     return{"total": tuple(result)[0][0]}
 
@@ -31,19 +31,31 @@ def get_users_count():
 def create_user(user: User):
     new_user = {"name": user.name, "email": user.email}
     new_user["password"] = f.encrypt(user.password.encode("utf-8"))
-    result = conn.execute(users.insert().values(new_user))
+    result = conn.execute(
+        users.insert()
+        .values(new_user)
+        )
     return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
 
 
-@user.put("/user/{id}/update")
-def update_user():
+@user.put("/user/{id}/update", response_model=User, description=" Update a User by Id")
+def update_user(user: User, id: int):
+    conn.execute(
+        users.update()
+        .values(name=user.name, email=user.email, password= user.password)
+        .where(users.c.id == id)
+    )
+    return conn.execute(users.select().where(user.c.id == id)).firts()
+
+
+
+@user.delete("/users/{id}/delete", tags=["users"], status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int):
+    conn.execute(
+        users.delete()
+        .where(users.c.id == id))
+    return conn.execute(users.select().where(users.c.id == id)).firts()
     
-
-
-
-@user.delete("/users/{id}/delete")
-def delete_user():
-    pass
 
 
 
