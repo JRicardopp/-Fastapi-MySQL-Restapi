be0c8db5a7b877a529e12382a9a6ac7a82e6dcb6
Jesus Pernia <padron2991@gmail.com>
2023-03-07 08:50:10 -0500

    path operation create user

--- config/db.py
+++ config/db.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymytsql://root:12020191@localhost:3306/crudb")
+engine = create_engine("mysql+pymysql://root:12020191@localhost:3306/crudb")
+
+meta = MetaData()
 
 conn = engine.connect()
+
+
--- routes/user.py
+++ routes/user.py
@@ -1,14 +1,33 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
+from fastapi import status
+
+from config.db import conn, engine
+from models.user import users
+from schemas.user import User
+
+
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+f = Fernet(key)
 
 user = APIRouter()
 
-@user.post("/users")
-def create_user():
-    pass
+@user.get("/users")
+def get_users():
+    return conn.execute(users.select()).fetchall()
 
-@user.get("/users/{id}")
-def show_user():
-    pass
+
+
+@user.post("/",
+           status_code=status.HTTP_201_CREATED
+           )
+def create_user(user: User):
+    new_user = {"name": user.name, "email": user.email}
+    new_user["password"] = f.encrypt(user.password.encode("utf-8"))
+    result = conn.execute(users.insert().values(new_user))
+    return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
 
 @user.delete("/users/{id}/delete")
 def delete_user():
@@ -17,3 +36,5 @@ def delete_user():
 @user.put("/user/{id}/update")
 def update_user():
     pass
+
+
